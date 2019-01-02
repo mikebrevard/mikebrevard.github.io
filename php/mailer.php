9189dcadf9807d712e0d370de3fdddbc16e5c2c0
Mike Brevard <mikebrevard@gmail.com>
2019-01-01 16:22:40 -0800

    adding first snapshot

---
+++ php/mailer.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(0);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name           = $_POST["name"];
+    $phone          = $_POST["phone"];
+    $email          = $_POST["email"];
+    $attendance     = $_POST["attendance"];
+    $guest          = $_POST["guest"];
+    $messages        = $_POST["messages"];
+
+
+    // Set the recipient email address.
+    // FIXME: Update this to your desired email address.
+    $recipient = "yourmail@example.com";
+
+    // Set the email subject.
+    $subject = "We are attending ( $name )";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Phone: $phone\n\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>".
+    'Reply-To: yourmail@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    // send
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        die("success");
+    } else {
+       die("fail");
+    }
+}
+?>
