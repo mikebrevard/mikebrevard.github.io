ae99b1397530959c125d0bc03be0f0822a7201ce
Mike Brevard <mikebrevard@gmail.com>
2019-01-12 19:08:22 -0800

    changed back to normal

--- php/mailer.php
+++ php/mailer.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $messages        = $_POST["messages"];
 
 
-    // Set the recipient email address.
+    // Set the recipient email address
     $recipient = "mikeandceleste2019@gmail.com";
 
     // Set the email subject.
@@ -27,33 +27,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     'X-Mailer: PHP/' . phpversion();
 
     // send
-    console.log($email_content)
-    // if (mail($recipient, $subject, $email_content, $email_headers)) {
-    //     die("success");
-    // } else {
-    //    die("fail");
-    // }
-
-    $mail = new PHPMailer(); // create a new object
-    $mail->IsSMTP(); // enable SMTP
-    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; // or 587
-    $mail->IsHTML(true);
-    $mail->Username = "mikeandceleste2019@gmail.com";
-    $mail->Password = "Wddng2019!";
-    $mail->SetFrom("mikeandceleste2019@gmail.com");
-    $mail->Subject = $subject;
-    $mail->Body = $email_content;
-    $mail->AddAddress($email);
-
-    if(!$mail->Send()) {
-      echo "Mailer Error: " . $mail->ErrorInfo;
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        die("success");
     } else {
-      echo "Message has been sent";
+       die("fail");
     }
-
 }
 ?>
