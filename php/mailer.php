12491d6f842b26690f3793d939c335c3ffc6aaff
Brevard <mbrevard@informatica.com>
2019-01-17 17:18:22 -0800

    another try

--- php/mailer.php
+++ php/mailer.php
@@ -15,40 +15,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Set the email subject.
     $subject = "We are attending ( $name )";
 
-    // Build the email content.
-    $email_content = "Name: $name\n";
-    $email_content .= "Phone: $phone\n\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
-
-    // using SendGrid's PHP Library
-    // https://github.com/sendgrid/sendgrid-php
-    // require 'vendor/autoload.php'; // If you're using Composer (recommended)
-    // Comment out the above line if not using Composer
-    require("./sendgrid-php/sendgrid-php/sendgrid-php.php");
-    // If not using Composer, uncomment the above line
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom($recipient, "Mike & Celeste");
-    $email->setSubject($subject);
-    $email->addTo($email, $name);
-    $email->addContent(
-        "text/plain", $email_content
-    );
-    $email->addContent(
-        "text/plain", "and easy to do anywhere, even with PHP"
-    );
-    $email->addContent(
-        "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-    );
-    $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-    try {
-        $response = $sendgrid->send($email);
-        print $response->statusCode() . "\n";
-        print_r($response->headers());
-        print $response->body() . "\n";
-    } catch (Exception $e) {
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    if (mail($recipient, $subject, $email_content)) {
+      die("success");
+    } else {
+       die("fail");
     }
+
+    // Build the email content.
+    // $email_content = "Name: $name\n";
+    // $email_content .= "Phone: $phone\n\n";
+    // $email_content .= "Email: $email\n\n";
+    // $email_content .= "Message:\n$message\n";
+    //
+    // // using SendGrid's PHP Library
+    // // https://github.com/sendgrid/sendgrid-php
+    // // require 'vendor/autoload.php'; // If you're using Composer (recommended)
+    // // Comment out the above line if not using Composer
+    // require("./sendgrid-php/sendgrid-php/sendgrid-php.php");
+    // // If not using Composer, uncomment the above line
+    // $email = new \SendGrid\Mail\Mail();
+    // $email->setFrom($recipient, "Mike & Celeste");
+    // $email->setSubject($subject);
+    // $email->addTo($email, $name);
+    // $email->addContent(
+    //     "text/plain", $email_content
+    // );
+    // $email->addContent(
+    //     "text/plain", "and easy to do anywhere, even with PHP"
+    // );
+    // $email->addContent(
+    //     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    // );
+    // $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+    // try {
+    //     $response = $sendgrid->send($email);
+    //     print $response->statusCode() . "\n";
+    //     print_r($response->headers());
+    //     print $response->body() . "\n";
+    // } catch (Exception $e) {
+    //     echo 'Caught exception: ',  $e->getMessage(), "\n";
+    // }
 
     // // Build the email headers.
     // $email_headers = "From: $name <$email>".
