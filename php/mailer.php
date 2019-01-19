29bf6f234d047536de385442cb3a87472dc6b19b
Mike Brevard <mikebrevard@gmail.com>
2019-01-19 14:31:07 -0800

    fixed the mailer

--- php/mailer.php
+++ php/mailer.php
@@ -14,16 +14,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $subject = "TODO";
     $email_content = "TODO";
-    $signature = "\r\n\r\nBest Wishes,\r\n\r\nMike and Celeste";
+    $signature = "\r\n\r\nBest Wishes,\r\nMike and Celeste";
 
-    if (startsWith($attendance,"Yes")) {
+    if (substr($attendance, 0, 3) === "Yes") {
       $subject = "$name is attending Mike and Celeste's Wedding!";
       $email_content = "Thank you for filling out the form on our site. We're excited that you'll be able to attend! We will send out an e-mail regarding hotel information and the dinner options for the event in the coming days. This is a summary of the information you provided in the form. $name will be attending with $guest guests. Their contact information is $phone and $email. They also mentioned that '$messages'$signature";
     } else {
-      if (startsWith($attendance,"Yes")) {
         $subject = "$name will not be able to attend Mike and Celeste's Wedding! =(";
         $email_content = "Thank you for filling out the form on our site. We're sad to hear you won't be able to attend our wedding. If your plans change, please let us know before March 20th.$signature";
-      }
     }
 
     if (mail($recipient, $subject, $email_content)) {
