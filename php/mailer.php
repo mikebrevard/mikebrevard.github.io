a9dbb2d8743ed2933a9f4bf5c77545696a59a5b0
Brevard <mbrevard@informatica.com>
2019-01-18 16:49:56 -0800

    removed the people, gallery sections. Added more information. Reworked the RSVP. Updated the mailer to have more i

--- php/mailer.php
+++ php/mailer.php
@@ -6,14 +6,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email          = $_POST["email"];
     $attendance     = $_POST["attendance"];
     $guest          = $_POST["guest"];
-    $messages        = $_POST["messages"];
+    $messages       = $_POST["messages"];
 
 
     // Set the recipient email address
-    $recipient = "mikeandceleste2019@gmail.com";
+    $recipient = "mikeandceleste2019@gmail.com, $email";
 
     // Set the email subject.
-    $subject = "We are attending ( $name )";
+    $subject = "Confirmation: $name is attending Mike and Celeste's Wedding!";
+
+    $email_content = "Thank you for being able to attend Celete and I's Wedding!\n";
+    $email_content .= "An additional e-mail will be sent to $email regarding information on hotel information and food options for the event\n";
+    $email_content .= "Below is the information from the form:\n";
+    $email_content .= "<b>Name</b>: "
+    $email_content .= $name;
+    $email_content .= "\n<b>Phone</b>: "
+    $email_content .= $phone;
+    $email_content .= "\n<b>E-mail</b>: "
+    $email_content .= $email;
+    $email_content .= "\n<b>Number of Guests</b>: "
+    $email_content .= $attendance;
+    $email_content .= "\n<b>Message</b>: "
+    $email_content .= $messages;
+    $email_content .= "\nBest,";
+    $email_content .= "\tMike";
 
     if (mail($recipient, $subject, $email_content)) {
       die("success");
@@ -21,51 +37,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
        die("fail");
     }
 
-    // Build the email content.
-    // $email_content = "Name: $name\n";
-    // $email_content .= "Phone: $phone\n\n";
-    // $email_content .= "Email: $email\n\n";
-    // $email_content .= "Message:\n$message\n";
-    //
-    // // using SendGrid's PHP Library
-    // // https://github.com/sendgrid/sendgrid-php
-    // // require 'vendor/autoload.php'; // If you're using Composer (recommended)
-    // // Comment out the above line if not using Composer
-    // require("./sendgrid-php/sendgrid-php/sendgrid-php.php");
-    // // If not using Composer, uncomment the above line
-    // $email = new \SendGrid\Mail\Mail();
-    // $email->setFrom($recipient, "Mike & Celeste");
-    // $email->setSubject($subject);
-    // $email->addTo($email, $name);
-    // $email->addContent(
-    //     "text/plain", $email_content
-    // );
-    // $email->addContent(
-    //     "text/plain", "and easy to do anywhere, even with PHP"
-    // );
-    // $email->addContent(
-    //     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-    // );
-    // $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-    // try {
-    //     $response = $sendgrid->send($email);
-    //     print $response->statusCode() . "\n";
-    //     print_r($response->headers());
-    //     print $response->body() . "\n";
-    // } catch (Exception $e) {
-    //     echo 'Caught exception: ',  $e->getMessage(), "\n";
-    // }
-
-    // // Build the email headers.
-    // $email_headers = "From: $name <$email>".
-    // 'Reply-To: mikeandceleste2019@gmail.com' . "\r\n" .
-    // 'X-Mailer: PHP/' . phpversion();
-    //
-    // // send
-    // if (mail($recipient, $subject, $email_content, $email_headers)) {
-    //     die("success");
-    // } else {
-    //    die("fail");
-    // }
 }
 ?>
