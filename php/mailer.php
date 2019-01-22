f317b0610e7823763fc4c10833db048c0631dd0b
Brevard <mbrevard@informatica.com>
2019-01-22 15:55:40 -0800

    updated RSVP form

--- php/mailer.php
+++ php/mailer.php
@@ -6,6 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email          = $_POST["email"];
     $attendance     = $_POST["attendance"];
     $guest          = $_POST["guest"];
+    $children       = $_POST["children"];
     $messages       = $_POST["messages"];
 
 
@@ -18,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (substr($attendance, 0, 3) === "Yes") {
       $subject = "$name is attending Mike and Celeste's Wedding!";
-      $email_content = "Thank you for filling out the form on our site. We're excited that you'll be able to attend! We will send out an e-mail regarding hotel information and the dinner options for the event in the coming days. This is a summary of the information you provided in the form. $name will be attending with $guest guests. Their contact information is $phone and $email. They also mentioned that '$messages'$signature";
+      $email_content = "Thank you for filling out the form on our site. We're excited that you'll be able to attend! We will send out an e-mail regarding hotel information and the dinner options for the event in the coming days. This is a summary of the information you provided in the form. $name will be attending with $guest guests and $children children. Their contact information is $phone and $email. They also mentioned that '$messages'$signature";
     } else {
         $subject = "$name will not be able to attend Mike and Celeste's Wedding! =(";
         $email_content = "Thank you for filling out the form on our site. We're sad to hear you won't be able to attend our wedding. If your plans change, please let us know before March 20th.$signature";
